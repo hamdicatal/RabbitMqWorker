82c4e61d134e43a152d62b5f33e85431adc203bf
hamdicatal <hamdicatal@outlook.com>
2023-04-26 23:35:55 +0300

    Add project files.

---
+++ ConsumerApp/Consumer.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ConsumerApp
+{
+    public class ResponseLog
+    {
+        public string ServiceName { get; set; } = "RabbitListener";
+        public string Url { get; set; }
+        public int StatusCode { get; set; }
+    }
+
+    static class Consumer
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "urls",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var url = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {url}");
+
+                var response = await HeadRequest(url);
+
+                LogResponse(url, response);
+
+            };
+            channel.BasicConsume(queue: "urls",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static void LogResponse(string url, HttpResponseMessage response)
+        {
+            ResponseLog responseLog = new ResponseLog();
+            responseLog.StatusCode = response.StatusCode.GetHashCode();
+            responseLog.Url = url;
+
+            string log = JsonSerializer.Serialize(responseLog);
+        }
+
+        private static async Task<HttpResponseMessage> HeadRequest(string url)
+        {
+            return await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
+        }
+    }
+}
---
+++ PublisherApp/Publisher.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace PublisherApp
+{
+    static class Publisher
+    {
+        static void PublishUrl(IModel channel, string url)
+        {
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "urls",
+                                 basicProperties: null,
+                                 body: Encoding.UTF8.GetBytes(url));
+
+            Console.WriteLine($" [x] Sent {url}");
+        }
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "urls",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            PublishUrl(channel, "https://jsonplaceholder.typicode.com/users");
+            // PublishUrl(channel, "https://twitter.com/");
+            // PublishUrl(channel, "https://youtube.com/");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+            
+        }
+    }
+}
